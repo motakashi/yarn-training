11b47043e25a3e97b1ca538edab519650553ac37
motakashi <takashi_home_sky@yahoo.co.jp>
2021-10-04 23:29:19 +0900

    パッケージ管理

---
+++ sum-app/app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const s = require('sum')
+const result = s.add([1,2,3,4]);
+console.log(result);
+
+const result2 = s.multi([1,2,3,4]);
+console.log(result2);
+
---
+++ sum/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+function add(numbers){
+    let result = 0;
+    for (const num of numbers) {
+        result = result + num;
+    }
+
+    return result;
+}
+
+function multi(numbers){
+    let result = 1;
+    for (const num of numbers) {
+        result = result * num;
+    }
+
+    return result;
+}
+
+module.exports = {
+    add: add,
+    multi: multi
+}
